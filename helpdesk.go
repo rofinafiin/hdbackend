0df7d9241222c88e033f98f636e2aa2fa86cec16
rofinafiin <rofinafiisr@gmail.com>
2023-03-19 18:04:20 +0700

    int to string

--- helpdesk.go
+++ helpdesk.go
@@ -89,7 +89,7 @@ func GetDataCompFromHandphone(phone string, db *mongo.Database, col string) (dat
 	return data
 }
 
-func GetDataJumlah(tahun int, db *mongo.Database, col string) (data []JumlahComplain) {
+func GetDataJumlah(tahun string, db *mongo.Database, col string) (data []JumlahComplain) {
 	user := db.Collection(col)
 	filter := bson.M{"tahun": tahun}
 	cursor, err := user.Find(context.TODO(), filter)
